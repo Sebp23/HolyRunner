c17de5187f4ad85964c321121030a83dd9d28375
Sebp23 <57201896+Sebp23@users.noreply.github.com>
2020-03-09 21:12:31 -0500

    Added score timer and worked with camer a little bit

---
+++ Assets/Scripts/ScoreCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreCamera : MonoBehaviour
+{
+    //failure gallery
+    //public Vector3 scorePos = new Vector3(0, 1, 0.25f);
+    //public Vector3 ViewportToWorldPoint(Vector3 scorepos);
+
+    //Vector3 scorePos = new Vector3(0, 1, 0.25f);
+    //transform.position = GetComponent<Camera>(ViewportToWorldPoint(scorePos));
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Camera.main.ViewportToWorldPoint(new Vector3(5, 0f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
---
+++ Assets/Scripts/ScoreTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTimer : MonoBehaviour
+{
+    private float timer = 0.0f;
+    public Text scoreTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AddSecond();
+    }
+
+    private void AddSecond()
+    {
+        timer += Time.deltaTime;
+        int score = Mathf.RoundToInt(timer);
+        scoreTime.text = ($"Score: {score.ToString()}");
+    }
+}
