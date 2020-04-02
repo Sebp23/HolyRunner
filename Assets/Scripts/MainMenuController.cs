c7626a31fdd96515238ae7b11285529be71a8bda
gwillemssen <57201850+gwillemssen@users.noreply.github.com>
2020-04-01 19:34:24 -0500

    Main Menu created

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+    public bool isCredits;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        if (isStart)
+        {
+            //Actual Game level is scene 1
+            SceneManager.LoadScene(1);
+            Debug.Log("Scene Loaded!");
+        }
+        if (isCredits)
+        {
+            //credits is scene 3
+            SceneManager.LoadScene(3);
+            Debug.Log("Scene Loaded!");
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+            Debug.Log("Scene Loaded!");
+        }
+    }
+}
